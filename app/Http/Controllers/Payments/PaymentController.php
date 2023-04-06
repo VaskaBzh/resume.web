6d01ca0d0297eec8b50e427420a5b99bee630738
Ailas Monsy <ognevoy.bima@gmail.com>
2023-04-06 17:46:37 +0300

    samiy noviy kontroller

--- app/Http/Controllers/Payments/PaymentController.php
+++ app/Http/Controllers/Payments/PaymentController.php
@@ -2,72 +2,60 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Subaccount;
-use App\Models\Transaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
-class AutomaticPayoutController extends Controller
+class PayoutController extends Controller
 {
+    /**
+     * Handle the payout process.
+     *
+     * @return void
+     */
     public function payout()
     {
         // Получаем все субаккаунты из базы данных
-        $subaccounts = Subaccount::all();
+        $subaccounts = DB::table('subaccounts')->get();
 
+        // Проверяем баланс субаккаунта и выполняем выплату, если баланс больше или равен указанному порогу
         foreach ($subaccounts as $subaccount) {
-            // Проверяем баланс субаккаунта
-            if ($subaccount->balance >= config('app.payout_threshold')) {
-                // Проводим выплату
-                $result = $this->makePayment($subaccount->bitcoin_address, $subaccount->balance);
+            if ($subaccount->balance >= 0.05) {
+                // Создаем новое соединение RPC-клиента Bitcoin
+                $bitcoin = new \Bitcoin\Rpc\Client(
+                    config('bitcoin.rpc_user'),
+                    config('bitcoin.rpc_password'),
+                    config('bitcoin.rpc_host'),
+                    config('bitcoin.rpc_port')
+                );
 
-                if ($result) {
-                    // Обновляем баланс субаккаунта
-                    $subaccount->balance = 0;
-                    $subaccount->save();
+                // Отправляем биткоины на указанный адрес из кошелька
+                $amount = $subaccount->balance;
+                $address = $subaccount->address;
+                $minconfirms = 6; // установка количества блоков
+                $txid = $bitcoin->sendtoaddress($address, $amount, '', '', true, null, $minconfirms);
 
-                    // Создаем запись транзакции в базе данных со статусом "выплачено"
-                    $transaction = new Transaction();
-                    $transaction->subaccount_id = $subaccount->id;
-                    $transaction->amount = $subaccount->balance;
-                    $transaction->status = 'выплачено';
-                    $transaction->save();
-                }
+                // Обновляем баланс субаккаунта и сохраняем транзакцию в базу данных
+                $subaccount->balance = 0;
+                $subaccount->save();
+
+                $transaction = new Transaction([
+                    'subaccount_id' => $subaccount->id,
+                    'amount' => $amount,
+                    'address' => $address,
+                    'txid' => $txid,
+                    'status' => 'Выплачено'
+                ]);
+                $transaction->save();
             } else {
-                // Создаем запись транзакции в базе данных со статусом "не оплачено"
-                $transaction = new Transaction();
-                $transaction->subaccount_id = $subaccount->id;
-                $transaction->amount = $subaccount->balance;
-                $transaction->status = 'не оплачено';
+                // Создаем запись транзакции в базе данных со статусом "Не оплачено"
+                $transaction = new Transaction([
+                    'subaccount_id' => $subaccount->id,
+                    'amount' => $subaccount->balance,
+                    'address' => $subaccount->address,
+                    'status' => 'Не оплачено'
+                ]);
                 $transaction->save();
             }
-        }
-    }
-
-    private function makePayment($bitcoinAddress, $amount)
-    {
-        // Логика для проведения выплаты на кошелек $bitcoinAddress
-        // Возвращает true при успешной выплате, false при ошибке
-        // Получаем данные о холодном кошельке
-        $cold_wallet = ColdWallet::first();
-        $rpc_user = $cold_wallet->rpc_user;
-        $rpc_password = $cold_wallet->rpc_password;
-        $rpc_port = $cold_wallet->rpc_port;
-        $rpc_host = $cold_wallet->rpc_host;
-
-        // Создаем новое соединение RPC-клиента Bitcoin
-        $bitcoin = new Bitcoin($rpc_user, $rpc_password, $rpc_host, $rpc_port);
-
-        // Проверяем баланс на кошельке
-        $balance = $bitcoin->getbalance();
-
-        if ($balance >= $amount) {
-            // Отправляем биткоины на указанный адрес
-            $txid = $bitcoin->sendtoaddress($address, $amount);
-
-            // Возвращаем true, чтобы показать, что выплата произведена успешно
-            return true;
-        } else {
-            // Возвращаем false, чтобы показать, что выплата не произведена из-за нехватки средств
-            return false;
         }
     }
 }
---
+++ config/bitcoin.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'rpc_user' => env('BITCOIN_RPC_USER', 'bituser'),
+    'rpc_password' => env('BITCOIN_RPC_PASSWORD', '111'),
+    'rpc_host' => env('BITCOIN_RPC_HOST', '92.205.163.43'),
+    'rpc_port' => env('BITCOIN_RPC_PORT', 8332),
+];
