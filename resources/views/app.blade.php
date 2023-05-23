cead9b080a50bff6043febd7bc690d07f563b04f
Vasily <vasily-bazhenov.03@mail.ru>
2023-05-23 17:10:01 +0300

    chgs

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
+    <meta name="yandex-verification" content="6700c3ddeecadb33" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="Зарабатывайте на майнинге вместе с allbtc pool. Allbtc poll - Высокий доход. Надежность. Эффективность.">
