67b38046768cb145356ad9a3728c3606eb1fa18d
gai_palyan <chadreed020788@yandex.ru>
2023-07-13 14:25:39 +0300

    fix: btcservice call remove

--- app/Services/External/BtcComService.php
+++ app/Services/External/BtcComService.php
@@ -9,31 +9,14 @@ use GuzzleHttp\Client;
 
 class BtcComService
 {
-    public function __construct(
-        private readonly Client $client,
-    )
-    {
-    }
-
     /**
      * Проверка на существование пользователя на стороне btc.com
      */
     public function btcHasUser($data): bool
     {
-        $data = $this->call(
-            method: 'get',
-            uri: implode('/', [
-                config('api.btc'),
-                'worker',
-                'groups'
-            ]) ,
-            options: []
-        );
-
         /* Убираем первые две группы ("Все группы", "Разгруппировано")
          * Проверяем наличие имени в группах
         */
-
         return collect($data['data']['list'])
             ->filter(static fn(array $groups, int $index) => $index > 1)
             ->pluck('name')
--- config/api.php
+++ config/api.php
@@ -3,7 +3,6 @@
 return [
     'btc' => [
         'uri' => env('BTC_API_URL', false),
-        'token' => env('')
     ],
     'minerstat' => env('MINERSTAT_API_URL', false),
 ];
