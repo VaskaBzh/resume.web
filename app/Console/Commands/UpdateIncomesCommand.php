38c9a56ed0f9e923236d1b1d7e9b958ae0edfce1
gai_palyan <chadreed020788@yandex.ru>
2023-07-13 15:54:32 +0300

    fix: update income if daily created

--- app/Console/Commands/UpdateIncomesCommand.php
+++ app/Console/Commands/UpdateIncomesCommand.php
@@ -109,11 +109,9 @@ class UpdateIncomesCommand extends Command
             ->orderByDesc('created_at')
             ->first();
 
-        if ($previousIncome?->created_at->diffInDays(now()) < 1) {
-            $previousIncome->update($income);
-        } else {
-            $sub->incomes()->create($income);
-        }
+        $previousIncome?->created_at->diffInDays(now()) < 1
+            ? $previousIncome->update($income)
+            : $sub->incomes()->create($income);
 
         $sub->accruals = $sumAccruals;
         $sub->unPayments = $sub->accruals - $sub->payments;
