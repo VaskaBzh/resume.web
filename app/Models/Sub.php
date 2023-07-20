06b3b9cd5dc638fefb3ef3941b4921da30018918
gai_palyan <chadreed020788@yandex.ru>
2023-07-20 18:37:25 +0300

    add: Finance entity, migrattion, percent column

--- app/Models/Sub.php
+++ app/Models/Sub.php
@@ -1,9 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Sub extends Model
 {
@@ -18,35 +22,40 @@ class Sub extends Model
         'payments',
         'unPayments',
         'accruals',
+        'percent',
     ];
 
-    public function user()
+    /* Relations */
+
+    public function finances(): HasMany
+    {
+        return $this->hasMany(Finance::class, 'group_id', 'group_id');
+    }
+
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    public function workers()
+    public function workers(): HasMany
     {
         return $this->hasMany(Worker::class, 'group_id', 'group_id');
     }
 
-    public function hashes()
+    public function hashes(): HasMany
     {
         return $this->hasMany(Hash::class, 'group_id', 'group_id');
     }
 
-    public function incomes()
+    public function incomes(): HasMany
     {
         return $this->hasMany(Income::class, 'group_id', 'group_id');
     }
 
-    public function wallets()
+    public function wallets(): HasMany
     {
         return $this->hasMany(Wallet::class, 'group_id', 'group_id');
     }
 
-//    public function clients()
-//    {
-//        return $this->hasMany(Client::class);
-//    }
+    /* end relations */
 }
