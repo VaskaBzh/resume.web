c809bf2e7645018e5e92d9af73a407b89eacfddb
Vasily <vasily-bazhenov.03@mail.ru>
2023-08-22 19:43:55 +0300

    pre fin settings

---
+++ resources/js/modules/settings/Components/blocks/SettingsPopup.vue
@@ -0,0 +1,111 @@
+<template>
+    <main-popup
+        id="changes"
+        class="popup-changes"
+        :wait="wait"
+        :closed="closed"
+        :errors="errors"
+    >
+        <form
+            @submit.prevent="
+                form.type === 'пароль'
+                    ? $emit('ajaxChange', true)
+                    : $emit('ajaxChange', false)
+            "
+            class="form form-popup popup__form"
+        >
+            <main-title tag="h3">{{
+                `${
+                    form.type !== "почту"
+                        ? $t("settings.block.settings_block.popup.title")
+                        : $t("settings.block.settings_block.popup.title_email")
+                } ${form.type}`
+            }}</main-title>
+            <input
+                v-model="item"
+                autofocus
+                :type="form.type === 'пароль' ? 'password' : 'text'"
+                class="input popup__input"
+                :placeholder="`${$t(
+                    'settings.block.settings_block.popup.placeholders.placeholder'
+                )} ${form.type}`"
+            />
+            <div class="form_row" v-if="form.type === 'пароль'">
+                <main-password
+                    name="password"
+                    :placeholder="`${$t(
+                        'settings.block.settings_block.popup.placeholders.password_new'
+                    )} ${form.type}`"
+                    :model="password"
+                    :errors="errors"
+                    @change="$emit('validate', $event)"
+                ></main-password>
+            </div>
+            <main-validate :validate="validate" />
+            <div class="form_row" v-if="form.type === 'пароль'">
+                <main-password
+                    name="password"
+                    :placeholder="`${$t(
+                        'settings.block.settings_block.popup.placeholders.password_confirmation'
+                    )} ${form.type}`"
+                    :model="password_confirmation"
+                    :errors="errors"
+                    @change="password_confirmation = $event"
+                ></main-password>
+            </div>
+            <blue-button>
+                <button type="submit" class="all-link">
+                    <popup-loading-icon />
+                    {{ $t("settings.block.settings_block.popup.button") }}
+                    {{ form.type }}
+                </button>
+            </blue-button>
+        </form>
+    </main-popup>
+</template>
+
+<script>
+import MainPopup from "@/Components/technical/MainPopup.vue";
+import MainPassword from "@/Components/UI/inputs/MainPassword.vue";
+import BlueButton from "@/Components/UI/BlueButton.vue";
+import MainValidate from "@/modules/common/Components/MainValidate.vue";
+import PopupLoadingIcon from "@/modules/common/icons/PopupLoadingIcon.vue";
+
+export default {
+    name: "settings-popup",
+    props: {
+        errors: Object,
+        form: Object,
+        validate: Object,
+        wait: Boolean,
+    },
+    components: {
+        MainPopup,
+        MainPassword,
+        BlueButton,
+        MainValidate,
+        PopupLoadingIcon,
+    },
+    data() {
+        return {
+            item: this.form.item,
+            password_confirmation: this.form.password_confirmation,
+            password: this.form.password,
+        };
+    },
+};
+</script>
+
+<style scoped lang="scss">
+.form_wrapper-message {
+    position: relative;
+    left: 0;
+    top: 0;
+    opacity: 1;
+    width: 100%;
+    .form_message {
+        opacity: 1;
+        width: 100%;
+    }
+}
+</style>
