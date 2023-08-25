6f1034063b39fa206e9325cd1e5f1ae4a543c971
gai_palyan <chadreed020788@yandex.ru>
2023-08-25 16:21:23 +0300

    add: validation password rule

---
+++ app/Rules/User/OnlyEngNameRule.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Rules\User;
+
+use Illuminate\Contracts\Validation\InvokableRule;
+use Illuminate\Contracts\Validation\Rule;
+
+class OnlyEngNameRule implements InvokableRule
+{
+    public function __invoke($attribute, $value, $fail)
+    {
+        if (!preg_match('/^[A-aZ-z0-9]+$/', $value)) {
+            $fail('Имя должно содержать только английские символы');
+        }
+    }
+}
