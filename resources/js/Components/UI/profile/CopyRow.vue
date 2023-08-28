a3b5234b4d12c7fbc3874553e8f508b7bc06e835
macman <vasily-bazhenov.03@mail.ru>
2023-08-28 12:29:34 +0300

    add images + animate + fix component

--- resources/js/Components/UI/profile/CopyRow.vue
+++ resources/js/Components/UI/profile/CopyRow.vue
@@ -7,29 +7,24 @@
             @click="this.copyLink"
         >
             {{ this.copyObject.link }}
-            <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
+            <copy-icon
                 class="copy_button"
                 :class="{ hide: active }"
-            >
-                <path
-                    d="M20 2H10C8.897 2 8 2.897 8 4V8H4C2.897 8 2 8.897 2 10V20C2 21.103 2.897 22 4 22H14C15.103 22 16 21.103 16 20V16H20C21.103 16 22 15.103 22 14V4C22 2.897 21.103 2 20 2ZM4 20V10H14L14.002 20H4ZM20 14H16V10C16 8.897 15.103 8 14 8H10V4H20V14Z"
-                    fill="#818C99"
-                />
-            </svg>
+            />
         </div>
     </div>
 </template>
 
 <script>
+import CopyIcon from "../../../modules/common/icons/CopyIcon.vue";
+
 export default {
     name: "copy-row",
     props: {
         copyObject: Object,
+    },
+    components: {
+        CopyIcon,
     },
     data() {
         return {
@@ -67,7 +62,7 @@ export default {
         transform: translateY(-50%);
         transition: all 0.3s ease 0s;
         opacity: 1;
-        fill: #343434;
+        stroke: #343434;
         @media (max-width: 478.98px) {
             right: 7px;
             width: 20px;
@@ -101,7 +96,7 @@ export default {
         &:hover {
             .copy {
                 &_button {
-                    fill: #4182ec !important;
+                    stroke: #4182ec !important;
                 }
             }
         }
