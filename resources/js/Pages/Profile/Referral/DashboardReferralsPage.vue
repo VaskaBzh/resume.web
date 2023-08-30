af6d2608603367163b3d305b60e4f0694cfb0234
Vasily <vasily-bazhenov.03@mail.ru>
2023-08-30 11:46:35 +0300

    add: add new pages

---
+++ resources/js/Pages/Profile/Referral/DashboardReferralsPage.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "DashboardReferralsPage"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/Profile/Referral/IncomeRefferralsPage.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "IncomeRefferralsPage"
+}
+</script>
+
+<style scoped>
+
+</style>
