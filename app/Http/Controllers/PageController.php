4a684e41cedca0b55f64973f8598e9b2e74eb96a
gai_palyan <chadreed020788@yandex.ru>
2023-08-31 15:36:57 +0300

    refactor: route dispatch referral

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -1,38 +1,37 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
-use App\Models\User;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Str;
 use Inertia\Inertia;
 
 class PageController extends Controller
 {
-
     public function index()
     {
         return Inertia::render(
-            component: Arr::get(config('inertia.components'), 'home', 'HomePage'),
-            props: [
-                'auth_user' => Auth::check(),
-                'user' => auth()->user()
-            ]
+            component: Arr::get(config('inertia.components'), 'home', 'HomePage')
         );
     }
 
     public function show(Request $request, string $page)
     {
-        return Inertia::render(
-            component: Arr::get(config('inertia.components'), $page, 'HomePage'),
-            props: [
-                'auth_user' => Auth::check(),
-                'user' => auth()->user()
-            ]
-        );
+        $queryable = $request->query('page');
+
+        return match (true) {
+            $page === 'profile' => redirect('/profile/statistic'),
+            $page === 'referral' && !$queryable => redirect('/referral?page=overview'),
+            default => Inertia::render(
+                component: Arr::get(config('inertia.components'), $queryable ?? $page, 'HomePage'),
+                props: [
+                    'auth_user' => Auth::check(),
+                    'user' => auth()->user()
+                ]
+            )
+        };
     }
 }
