4b7424dd7fc5a72bcd3d150ed5ec9f89a7241118
gai_palyan <chadreed020788@yandex.ru>
2023-08-31 14:59:23 +0300

    refactor: remove page route methods

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -14,7 +14,18 @@ use Inertia\Inertia;
 class PageController extends Controller
 {
 
-    public function __invoke(Request $request, string $page)
+    public function index()
+    {
+        return Inertia::render(
+            component: Arr::get(config('inertia.components'), 'home', 'HomePage'),
+            props: [
+                'auth_user' => Auth::check(),
+                'user' => auth()->user()
+            ]
+        );
+    }
+
+    public function show(Request $request, string $page)
     {
         return Inertia::render(
             component: Arr::get(config('inertia.components'), $page, 'HomePage'),
@@ -24,88 +35,4 @@ class PageController extends Controller
             ]
         );
     }
-
-    /*public function index()
-    {
-        return Inertia::render('HomePage', [
-            'title' => 'Allbtc',
-            'auth_user' => Auth::check(),
-            'user' => auth()->user()
-        ]);
-    }
-
-    public function confirm()
-    {
-        return Inertia::render('Auth/ConfirmPage', [
-            'auth_user' => Auth::check(),
-            'user' => auth()->user()
-        ]);
-    }
-
-    public function login()
-    {
-        return Inertia::render('Auth/LoginPage', [
-            'auth_user' => Auth::check(),
-            'user' => auth()->user()
-        ]);
-    }
-
-    public function registration()
-    {
-        return Inertia::render('Auth/RegPage', [
-            'auth_user' => Auth::check(),
-            'user' => auth()->user()
-        ]);
-    }
-
-    public function hosting()
-    {
-        return Inertia::render('HostingPage', [
-            'auth_user' => Auth::check(),
-            'user' => auth()->user()
-        ]);
-    }
-
-    public function help()
-    {
-        return Inertia::render('FaqPage', [
-            'auth_user' => Auth::check(),
-            'user' => auth()->user()
-        ]);
-    }
-
-    public function calculator()
-    {
-        return Inertia::render('CalculatorPage', [
-            'auth_user' => Auth::check(),
-            'user' => auth()->user()
-        ]);
-    }
-
-    public function settings()
-    {
-        $user = auth()->user();
-
-        $ownerUser = User::whereNotNull('referral_code')
-            ->find($user?->owners->first()?->user_id);
-
-        return Inertia::render('Profile/FullPages/SettingsPage', [
-            'auth_user' => Auth::check(),
-            'user' => $user,
-            'referral_code' => $ownerUser?->referral_code['code']
-        ]);
-    }
-
-    public function twoFactorAuth()
-    {
-        $rendered = Inertia::render('Auth/TwoFactorVerifyPage', [
-            'qrCode' => session()->get('qrCode'),
-            'secret' => session()->get('secret')
-        ]);
-
-        session()->forget('qrCode');
-        session()->forget('secret');
-
-        return $rendered;
-    }*/
 }
