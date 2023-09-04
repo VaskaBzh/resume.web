9bd8c09fe88e8988ab0513c892b4fd3b52e7ffa4
gai_palyan <chadreed020788@yandex.ru>
2023-09-04 14:06:02 +0300

    refactor: make dev_common actual congif

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,11 +4,8 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Str;
 use Inertia\Inertia;
 
 class PageController extends Controller
@@ -16,14 +13,14 @@ class PageController extends Controller
     public function index()
     {
         return Inertia::render(
-            component: Arr::get(config('inertia.components'), 'home', 'HomePage')
+            component: Arr::get(config('inertia.components.public'), 'home', 'HomePage')
         );
     }
 
     public function show(Request $request, string $page)
-    {
+    {/*dd(Arr::get(config('inertia.components.public'), $queryable ?? $page));*/
         Inertia::render(
-            component: Arr::get(config('inertia.components'), $queryable ?? $page, 'HomePage'),
+            component: Arr::get(config('inertia.components.public'), $queryable ?? $page),
             props: [
                 "token" => csrf_token(),
             ]
