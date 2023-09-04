e2d013886c209a40a18f7671bfcf413a3b267377
gai_palyan <chadreed020788@yandex.ru>
2023-09-04 14:06:02 +0300

    refactor: make dev_common actual congif

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,12 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Str;
 use Inertia\Inertia;
 
 class PageController extends Controller
@@ -16,18 +13,14 @@ class PageController extends Controller
     public function index()
     {
         return Inertia::render(
-            component: Arr::get(config('inertia.components'), 'home', 'HomePage'),
-            props: [
-                'auth_user' => Auth::check(),
-                'user' => auth()->user()
-            ]
+            component: Arr::get(config('inertia.components.public'), 'home', 'HomePage')
         );
     }
 
     public function show(Request $request, string $page)
-    {
+    {/*dd(Arr::get(config('inertia.components.public'), $queryable ?? $page));*/
         Inertia::render(
-            component: Arr::get(config('inertia.components'), $queryable ?? $page, 'HomePage'),
+            component: Arr::get(config('inertia.components.public'), $queryable ?? $page),
             props: [
                 "token" => csrf_token(),
             ]
--- app/Http/Middleware/Profile/RedirectMiddleware.php
+++ app/Http/Middleware/Profile/RedirectMiddleware.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Str;
 class RedirectMiddleware
 {
     public function handle(Request $request, Closure $next)
-    {
+    {dd($request);
         return match ($request->getRequestUri()) {
             //'/profile' => redirect('/profile/statistic'),
             '/referral' => redirect('/profile/referral?page=overview'),
