bacd6b10b21be351ec7d78a487e8f1509d72bf12
gai_palyan <chadreed020788@yandex.ru>
2023-09-07 18:36:52 +0300

    fix: uncomment web routes #5

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -35,8 +35,8 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/auth.php'))
                 ->group(base_path('routes/api.php'));
 
-            /*Route::middleware('web')
-                ->group(base_path('routes/web.php'));*/
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
         });
     }
 
