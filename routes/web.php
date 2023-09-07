887f935f46956babc36bac955a3c343f0c2f6792
gai_palyan <chadreed020788@yandex.ru>
2023-09-07 18:18:03 +0300

    refactor: replace all routes to api

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', \App\Http\Controllers\HomeController::class)->name('home');
 
 /* Must auth web routes */
 Route::middleware('auth')->group(function () {
