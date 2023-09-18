3807dc75a729b4a965c2f8be3788d4701db012a5
macman <vasily-bazhenov.03@mail.ru>
2023-09-18 10:21:50 +0300

    rollback after rebase

--- database/factories/MinerStatFactory.php
+++ database/factories/MinerStatFactory.php
@@ -13,19 +13,6 @@ class MinerStatFactory extends Factory
     public function definition(): array
     {
         return [
-<<<<<<< HEAD
-            'network_hashrate' => 369.60,
-            'network_unit' => 'E',
-            'network_difficulty' => 55621444139429,
-            'next_difficulty' => 54452584425134,
-            'change_difficulty' => '-2.1%',
-            'reward_block' => 6.2500000,
-            'price_USD' => 26081,
-            'time_remain' => 1694024386000,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-            'fpps_rate' => 1.76,
-=======
             'network_hashrate' => $this->faker->randomFloat(),
             'network_unit' => $this->faker->word(),
             'network_difficulty' => $this->faker->randomNumber(),
@@ -37,7 +24,6 @@ class MinerStatFactory extends Factory
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
             'fpps_rate' => $this->faker->randomFloat(),
->>>>>>> 060e7a1 (pulled)
         ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -30,22 +30,6 @@ Route::middleware('auth')->group(function () {
 //        Route::post('verify', [TwoFactorController::class, 'verify'])->name('2fa.verify');
 //    });
     Route::post('/send_message', SendMessageConroller::class)->name('send_message');
-<<<<<<< HEAD
-=======
-
-    Route::group([
-        'prefix' => 'referrals'
-    ], function () {
-        Route::get('', [IndexController::class, 'referral'])->name('referral');
-        Route::get('/attached-referrals', [IndexController::class, 'referral'])->name('referral.attached');
-        Route::get('/incomes', [IndexController::class, 'referral'])->name('referral.incomes');
-        Route::get('{user}', ListReferralController::class)->name('referral.list');
-        Route::post('/generate/{user}', CodeController::class)->name('code');
-        Route::get('/statistic/{user}', StatisticReferralController::class)->name('referral.show');
-        Route::get('/incomes/{user}', ReferralIncomeListController::class)->name('referral.income.list');
-        Route::post('/attach/{user}', AttachReferralController::class)->name('referral.attach');
-    });
->>>>>>> 344af8d (refactor: referrals routes)
 });
 
 Route::get('/verify/{id}/{hash}', VerificationController::class)->name('verification.verify');
