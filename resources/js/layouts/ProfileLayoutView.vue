43c479101c85bebfb2ce0b635d869438b7398a87
Элина Резенькова <elina@MacBook-Pro-Elina.local>
2023-09-19 11:14:57 +0300

    feat: change layout view to relative position. #8

--- resources/js/layouts/ProfileLayoutView.vue
+++ resources/js/layouts/ProfileLayoutView.vue
@@ -1,16 +1,20 @@
 <template>
     <div class="layout">
-        <div class="header-container">
-            <header-component-profile :user="user" />
-        </div>
         <div class="nav-container">
             <nav-tabs ref="tabs" :user="user" />
         </div>
-        <div class="page-container">
-            <keep-alive>
-                <slot />
-            </keep-alive>
+        <div>
+            <div class="header-container">
+            <header-component-profile :user="user" />
+            </div>
+
+            <div class="page-container">
+                <keep-alive>
+                    <slot />
+                </keep-alive>
+            </div>
         </div>
+
     </div>
 </template>
 <script>
@@ -47,27 +51,21 @@ export default {
     width: 100vw;
     height: 100vh;
     background: var(--secondary-white, #fff);
+    display: flex;
 }
 .header-container {
     width: calc(100vw - 320px);
     height: 84px;
-    position: fixed;
     display: flex;
     align-items: center;
-    top: 0;
-    right: 0;
     transition: all 0.3s ease 0s;
 }
 .nav-container {
     width: 320px;
     height: 100vh;
     padding: 0 24px;
-    position: fixed;
-    top: 0px;
-    left: 0px;
 }
 .page-container {
-    position: fixed;
     padding: 24px;
     border-radius: 40px 0px 0px 0px;
     background: var(--background-first, #f8fafd);
