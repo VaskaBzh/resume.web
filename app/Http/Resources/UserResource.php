a786b58f92148004c8f0d4d22323306da52a3b1f
gai_palyan <chadreed020788@yandex.ru>
2023-09-26 19:28:20 +0300

    fix: add roles to user resourse

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -10,6 +10,7 @@ class UserResource extends JsonResource
     public function toArray($request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
             'email_verified_at' => $this->email_verified_at,
