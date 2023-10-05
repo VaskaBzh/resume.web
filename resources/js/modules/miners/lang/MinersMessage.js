538645877384d6a326c496b271378fad500357b2
Eelllllyy <r-elina-r@yandex.ru>
2023-10-05 15:08:28 +0300

    feat: add lang files. #8

---
+++ resources/js/modules/miners/lang/MinersMessage.js
@@ -0,0 +1,116 @@
+export const MinersMessage = {
+    en: {
+        title: ['transparent', 'and efficient', 'mining', 'bitcoin'],
+        buttun: 'get advice',
+        how_are_we: {
+            title: ['mining pool and', 'community for', 'miners'],
+            column:{
+                num: ['>3', '4%', '>1.7'],
+                gray_text: ['years', '', 'EH /s'],
+                main_text: ['We work in the market', 'digital assets', 'permanent', 'commission', 'Total hashrate', 'Allbtc Pool']
+            }
+        },
+        offer: {
+            title: 'what we offer',
+            text: 'Efficient and stable mining pool with personal commission, optimized for Bitcoin mining. As well as a set of tools for a transparent and convenient analysis of your assets.'
+        },
+        personal_account:{
+            title: 'convenient personal account',
+            text: 'Convenient personal account with all the necessary statistics and intuitive navigation. Only the necessary functions for the end user. ',
+            button: 'try personal account'
+        },
+        mobile_app: {
+            title: 'mobile application',
+            text: 'Optimized and intuitive mobile application. Hashrate monitoring, notifications and access to income information at any time on your phone.',
+            button: 'coming soon to the appstore'
+        },
+        guarantees: {
+            title: 'Guarantees of reliability and safety',
+            text: 'A personal manager will work with you who will help resolve any issues, including questions from your clients. We value our clients and strive to provide the best experience with allbtc pool',
+            list: {
+                title: ['FPPS+ payment system', 'security guarantee', 'automatic payments', 'prompt technical support', 'timely notifications'],
+                text: ['The most advanced reward method with the highest payout as it combines block reward and transaction fee payments into one value. The pool pays rewards regardless of whether a block is found.']
+            }
+        },
+        community: {
+            title: 'Large mining community and industry news',
+            text: 'Join the community of like-minded professionals and subscribe to our blogs for the Russian-speaking audience.',
+            list: {
+                title: ['join the community', 'read our blogs'],
+                buttons: ['Telegram community', 'telegram channel']
+            }
+        },
+        connect: {
+            title: 'interested in working with us?',
+            text: 'Leave your phone number, the client manager will contact you and answer all your questions.',
+            button: 'contact us',
+            form: {
+                placeholder: 'Enter a name',
+                button: ['request a call', 'TELEGRAM', 'what’s app'],
+                text: 'or'
+            }
+        },
+        footer: {
+            button: 'Personal account',
+            list: ['Home', 'Data centers', 'Miners', 'FAQ', 'News', 'Contacts'],
+            text: 'Privacy Policy'
+        }
+    },
+    ru: {
+        title: ['прозрачный', 'и эффективный', 'майнинг', 'bitcoin'],
+        buttun: 'получить консультацию',
+        how_are_we: {
+            title: ['майнинг пул и', 'комьюнити для', 'майнеров'],
+            column:{
+                num: ['>3', '4%', '>1,7'],
+                gray_text: ['лет', '', 'EH /s'],
+                main_text: ['Работаем на рынке', 'цифровых активов', 'постоянная', 'комиссия', 'Общий хешрейт', 'Allbtc Pool']
+            }
+        },
+        offer: {
+            title: 'что мы предлагаем',
+            text: 'Эффективный и стабильный майнинг-пул с персональной комиссией, оптимизированный под добычу Bitcoin. А также комплекс инструментов для прозрачного и удобного анализа ваших активов.'
+        },
+        personal_account:{
+            title: 'удобный личный кабинет',
+            text: 'Удобный личный кабинет со всей необходимой статистикой с интуитивно понятной навигацией. Только нужные функции для конечного пользователя. ',
+            button: 'попробовать личный кабинет'
+        },
+        mobile_app: {
+            title: 'мобильное приложение',
+            text: 'Оптимизированное и интуитивно понятное мобильное приложение. Мониторинг хэшрейта,  уведомления и доступ к инфо о доходах в любой момент в вашем телефоне.',
+            button: 'скоро в appstore'
+        },
+        guarantees: {
+            title: 'Гарантии надежности и безопасности',
+            text: 'С вами будет работать персональный менеджер, который поможет решить любые вопросы, в том числе вопросы ваших клиентов мы ценим наших клиентов и стремимся обеспечить лучший опыт работы с allbtc pool',
+            list: {
+                title: ['система выплаты FPPS+', 'гарантия безопасности', 'автовыплаты', 'оперативная техподдержка', 'своевременные уведомления'],
+                text: ['Самый современный метод вознаграждения с наибольшей выплатой, поскольку объединяет вознаграждение за блок и выплаты комиссий за транзакции в одно значение. Пул выплачивает вознаграждение независимо от нахождения блока.']
+            }
+        },
+        community: {
+            title: 'Большое комьюнити майнеров и новости индустрии',
+            text: 'Присоединяйтесь к комьюнити профессионалов- единомышленников и подписывайтесь на наши блоги для русскоязычной аудитории.',
+            list: {
+                title: ['вступайте в комьюнити', 'читайте наши блоги'],
+                buttons: ['Telegram комьюнити', 'telegram канал']
+            }
+        },
+        connect: {
+            title: 'интересно поработать с нами?',
+            text: 'Оставьте ваш номер телефона, клиентский менеджер свяжется с вами и ответит на все возникшие вопросы.',
+            button: 'свяжитесь с нами',
+            form: {
+                placeholder: 'Введите имя',
+                button: ['заказать звонок', 'TELEGRAM', 'what’s app'],
+                text: 'или'
+            }
+        },
+        footer: {
+            button: 'Личный кабинет',
+            list: ['Главная', 'Дата-центрам', 'Майнерам', 'FAQ', 'Новости', 'Контакты'],
+            text: 'Политика конфиденциальности'
+        }
+    },
+};
