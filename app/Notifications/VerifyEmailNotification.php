a69983572f065e6bc7218db02040d4e4faf9a87e
macman <vasily-bazhenov.03@mail.ru>
2023-10-09 20:56:46 +0300

    fix: restore back

--- app/Notifications/VerifyEmailNotification.php
+++ app/Notifications/VerifyEmailNotification.php
@@ -48,13 +48,13 @@ class VerifyEmailNotification extends VerifyEmail
     {
         $mail = (new MailMessage)
             ->view('mail.user.email-verify')
-            ->greeting(__('notification.greetings', ['email' => $notifiable->email]));
+            ->greeting(__('notifications.greetings', ['email' => $notifiable->email]));
 
         return match ($this->actionRoute) {
             'v1.verification.verify' => $this->getEmailVerifyMailMessage($mail, $notifiable)
-                ->line(__('notification.email.expired_at.text', ['value' => config('auth.verification.expire') / 60])),
+                ->line(__('notifications.email.expired_at.text', ['value' => config('auth.verification.expire') / 60])),
             'v1.password.reset.verify' => $this->getPasswordChangeMailMessage($mail, $notifiable)
-                ->line(__('notification.email.expired_at.text', ['value' => config('auth.verification.expire') / 60])),
+                ->line(__('notifications.email.expired_at.text', ['value' => config('auth.verification.expire') / 60])),
             default => throw new \Exception('Wrong route action ' . $this->actionRoute)
         };
     }
@@ -62,9 +62,9 @@ class VerifyEmailNotification extends VerifyEmail
     public function getEmailVerifyMailMessage(Renderable $mail, $notifiable): MailMessage
     {
         return $mail
-            ->line(__('notification.email.verify.context'))
+            ->line(__('notifications.email.verify.context'))
             ->action(
-                text: __('notification.email.verify.action-text'),
+                text: __('notifications.email.verify.action-text'),
                 url: $this->verificationEmailUrl(
                     $notifiable,
                     config('auth.verification.expire')
@@ -75,9 +75,9 @@ class VerifyEmailNotification extends VerifyEmail
     public function getPasswordChangeMailMessage(Renderable $mail, $notifiable): MailMessage
     {
         return $mail
-            ->line(__('notification.email.password-reset.context'))
+            ->line(__('notifications.email.password-reset.context'))
             ->action(
-                text: __('notification.email.password-reset.action-text'),
+                text: __('notifications.email.password-reset.action-text'),
                 url: $this->verificationEmailUrl(
                     $notifiable,
                     config('auth.passwords.users.expire')
