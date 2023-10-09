fb251b458efa3dddf973170741cb14da69ff0bb2
gai_palyan <chadreed020788@yandex.ru>
2023-10-09 14:02:50 +0300

    fix: incomes messages #34

---
+++ lang/en/statuses.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'less minWithdrawal' => 'Insufficient funds for withdrawal',
+    'completed' => 'Payout successfully completed',
+    'error payout' => 'An error occurred while making the payout',
+    'no wallet' => 'Configure your account for withdrawal (enter the wallet)',
+];
