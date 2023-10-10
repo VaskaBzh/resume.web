a0711a0e2b70bcaf42e771c6cde8173dead00c10
GlazGo31 <job.glazunov@gmail.com>
2023-10-10 17:13:20 +0300

    feat: scrolltriger miners

---
+++ resources/js/modules/home/Components/Views/HistoryPoolView.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="history-pool">
+    <head-line class="history-pool__btn">История нашего пула</head-line>
+
+</div>
+</template>
+
+
+
+<script>
+import HeadLine from "../../../common/Components/UI/HeadLine.vue";
+
+export default {
+    name: "HistoryPoolView",
+    components: {HeadLine}
+}
+</script>
+
+
+
+<style scoped lang="scss">
+
+</style>
