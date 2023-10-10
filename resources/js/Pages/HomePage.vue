6899b505da37a0a6856792554afcfa1d5a5601c6
GlazGo31 <job.glazunov@gmail.com>
2023-10-10 19:47:03 +0300

    fix: changes

--- resources/js/Pages/HomePage.vue
+++ resources/js/Pages/HomePage.vue
@@ -1,11 +1,11 @@
 <template>
-	<logo-animate-view/>
-<!--    <div class="app_back_transparent">-->
-<!--        <div class="app_back_transparent__container">-->
-<!--            <div class="app_back_transparent_block-one"><span class="app_back_transparent_one">allbtc</span></div>-->
-<!--            <div class="app_back_transparent_block-two"><span class="app_back_transparent_two">pool</span></div>-->
-<!--        </div>-->
-<!--    </div>-->
+    <header-component/>
+    <div class="app_back_transparent">
+        <div class="app_back_transparent__container">
+            <div class="app_back_transparent_block-one"><span class="app_back_transparent_one">allbtc</span></div>
+            <div class="app_back_transparent_block-two"><span class="app_back_transparent_two">pool</span></div>
+        </div>
+    </div>
     <landing-title/>
     <who-we-are/>
     <calculator-land/>
@@ -13,46 +13,39 @@
     <make-up-cab/>
     <security-view/>
     <app-mobile-view/>
-    <payments-view/>
+    <payments-view />
     <mission-view/>
-    <history-pool-view/>
     <connect-with-us-view/>
-    <footer-component-land/>
 </template>
 <script>
 
-import HeaderComponent from "@/modules/common/Components/HeaderComponent.vue";
-import LandingTitle from "@/modules/common/Components/UI/LandingTitle.vue";
-import WhoWeAre from "@/modules/home/Components/Views/WhoWeAre.vue";
-import CalculatorLand from "@/modules/home/Components/Views/CalculatorLand.vue";
-import MinersInfoView from "@/modules/home/Components/Views/MinersInfoView.vue";
-import MakeUpCab from "@/modules/home/Components/Views/MakeUpCab.vue";
-import SecurityView from "@/modules/home/Components/Views/SecurityView.vue";
-import AppMobileView from "@/modules/home/Components/Views/AppMobileView.vue";
-import PaymentsView from "@/modules/home/Components/Views/PaymentsView.vue";
-import MissionView from "@/modules/home/Components/Views/MissionView.vue";
-import ConnectWithUsView from "@/modules/home/Components/Views/ConnectWithUsView.vue";
-import FooterComponentLand from "@/modules/common/Components/FooterComponentLand.vue";
-import {animateHeader} from "@/modules/home/Services/LandHeaderAnime";
-import LogoAnimateView from "../modules/home/Components/Views/LogoAnimateView.vue";
+import HeaderComponent from "../modules/common/Components/HeaderComponent.vue";
+import LandingTitle from "../modules/common/Components/UI/LandingTitle.vue";
+import WhoWeAre from "../modules/home/Components/Views/WhoWeAre.vue";
+import CalculatorLand from "../modules/home/Components/Views/CalculatorLand.vue";
+import MinersInfoView from "../modules/home/Components/Views/MinersInfoView.vue";
+import MakeUpCab from "../modules/home/Components/Views/MakeUpCab.vue";
+import SecurityView from "../modules/home/Components/Views/SecurityView.vue";
+import AppMobileView from "../modules/home/Components/Views/AppMobileView.vue";
+import PaymentsView from "../modules/home/Components/Views/PaymentsView.vue";
+import MissionView from "../modules/home/Components/Views/MissionView.vue";
+import ConnectWithUsView from "../modules/home/Components/Views/ConnectWithUsView.vue";
+import FooterComponentLand from "../modules/common/Components/FooterComponentLand.vue";
+import {animateHeader} from "../modules/home/Services/LandHeaderAnime";
+
 
 export default {
-    name: 'home-page',
+    name: 'homa-page',
     components: {
-	    LogoAnimateView,
-        HistoryPoolView,
         FooterComponentLand,
         ConnectWithUsView,
         MissionView,
         PaymentsView,
         AppMobileView,
-        SecurityView,
-	    MakeUpCab,
-	    MinersInfoView,
-	    CalculatorLand,
-	    WhoWeAre,
-	    LandingTitle,
-	    HeaderComponent
+        SecurityView, MakeUpCab, MinersInfoView, CalculatorLand, WhoWeAre, LandingTitle, HeaderComponent},
+
+    data() {
+
     },
     mounted() {
         animateHeader()
@@ -82,17 +75,17 @@ export default {
             justify-content: center;
             flex-flow: column nowrap;
             font-feature-settings: 'clig' off, 'liga' off;
-	        text-shadow: 0px 5.13397216796875px 8.984451293945312px rgba(14, 14, 14, 0.05);
+            text-shadow: 0px 4.914285659790039px 8.600000381469727px rgba(14, 14, 14, 0.05);
             font-family: Unbounded, serif;
             color: transparent;
-            //-webkit-text-stroke: 1px white;
+            -webkit-text-stroke: 1px white;
             font-size: 290px;
             font-style: normal;
             font-weight: 600;
             -webkit-background-clip: text;
 
 
-            //-webkit-text-fill-color: rgb(255,255,255,0.7);
+            -webkit-text-fill-color: rgb(255,255,255,0.7);
             opacity: .7;
 
             line-height: 120%;
@@ -123,7 +116,6 @@ export default {
         &_one, &_two {
             display: block;
             padding: 3px;
-	        text-shadow: 0px 5.13397216796875px 8.984451293945312px rgba(14, 14, 14, 0.05);
         }
 
     }
