bd1478eddbd6536683464dffce0f02b147884646
macman <vasily-bazhenov.03@mail.ru>
2023-10-11 15:19:30 +0300

    refactor: title && headline components

---
+++ resources/js/modules/common/Components/UI/LandingHeadline.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="headline">
+        <span class="headline_text">
+            <slot />
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "landing-headline",
+};
+</script>
+
+<style scoped>
+.headline {
+    border-radius: 30px;
+    border: 0.5px solid var(--secondary-gray, #98a2b3);
+    padding: 4px 24px;
+    min-height: 52px;
+    width: fit-content;
+    display: inline-flex;
+    align-items: center;
+}
+.headline_text {
+    color: var(--gray-3100, #D0D5DD);
+    font-family: Unbounded, serif;
+    font-size: 20px;
+    font-weight: 600;
+    line-height: 120%;
+    text-transform: uppercase;
+    white-space: nowrap;
+}
+</style>
