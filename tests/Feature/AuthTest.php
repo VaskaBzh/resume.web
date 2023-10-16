bbf728010ba0ee57564ded7324aca278f34676d3
gai_palyan <chadreed020788@yandex.ru>
2023-10-16 18:50:49 +0300

    fix: code validation

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -3,10 +3,13 @@
 namespace Tests\Feature;
 
 use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_login()
     {
         dd(User::all());
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,13 +2,11 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-    use RefreshDatabase;
 
     public function test_is_testing_environment()
     {
