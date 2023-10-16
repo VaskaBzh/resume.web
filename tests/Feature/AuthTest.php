329f8f84ca2f5259f2b1f4a769f05e999360a532
gai_palyan <chadreed020788@yandex.ru>
2023-10-16 18:29:54 +0300

    fix: api docs

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -3,23 +3,12 @@
 namespace Tests\Feature;
 
 use App\Models\User;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
 {
-
-    private User $user;
-
-    protected function setUp(): void
+    public function test_login()
     {
-        $this->user = User::factory()->create();
         dd(User::all());
     }
-
-//    public function test_login()
-//    {
-//        dd($this->user);
-//    }
 }
