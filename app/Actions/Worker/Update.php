ddc21a1485a3f6bb8e185d43394de36750087b1d
gai_palyan <chadreed020788@yandex.ru>
2023-10-18 18:50:59 +0300

    fix: worker sync from btc.com, user resource, registr

--- app/Actions/Worker/Update.php
+++ app/Actions/Worker/Update.php
@@ -1,8 +1,25 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Actions\Worker;
+
+use App\Dto\WorkerData;
+use App\Models\Worker;
 
 class Update
 {
-
+    public static function execute(WorkerData $workerData): void
+    {
+        Worker::where('worker_id', $workerData->worker_id)
+            ->withTrashed()
+            ->update([
+                'name' => $workerData->name,
+                'approximate_hash_rate' => $workerData->approximateHashRate,
+                'status' => $workerData->status,
+                'unit' => $workerData->unit,
+                'pool_data' => $workerData->poolData,
+                'deleted_at' => null,
+            ]);
+    }
 }
