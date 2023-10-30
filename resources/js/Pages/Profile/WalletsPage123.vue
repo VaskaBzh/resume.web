4c930759e3784c3c81d710219c37add9ab7a1b9b
macman <vasily-bazhenov.03@mail.ru>
2023-10-30 11:10:59 +0300

    refactor: return old wallet page

---
+++ resources/js/Pages/Profile/WalletsPage123.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="wallets">
+        <!--        <main-preloader />-->
+        <div class="wallets__block">
+            <no-wallets-block
+                time="00.00.05"
+                @addWallet="service.openAddPopup()"
+            />
+        </div>
+        <add-wallet-popup
+            :is-opened="service.isAddPopupOpened.state"
+            @closed="closeAddPopup"
+            @addWallet="console.log(123)"
+        />
+    </div>
+</template>
+
+<script>
+// import MainPreloader from "@/modules/preloader/Components/MainPreloader.vue";
+import NoWalletsBlock from "@/modules/wallets/Components/NoWalletsBlock.vue";
+import AddWalletPopup from "@/modules/wallets/Components/AddWalletPopup.vue";
+
+import { WalletsService } from "@/modules/wallets/services/WalletsService";
+
+export default {
+    name: "WalletsPage",
+    components: {
+        // MainPreloader,
+        NoWalletsBlock,
+        AddWalletPopup,
+    },
+    data() {
+        return {
+            service: new WalletsService(),
+        };
+    },
+    methods: {
+        closeAddPopup() {
+            this.service.closeAddPopup().setForm().closeAddEmailForm();
+        },
+    },
+};
+</script>
+
+<style scoped>
+.wallets {
+    flex: 1 1 auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
