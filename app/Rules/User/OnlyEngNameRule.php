7cd285910904b106236fff5144e83abfaa05aa72
gai_palyan <chadreed020788@yandex.ru>
2023-10-30 16:29:05 +0300

    fix: validation response lang #73

--- app/Rules/User/OnlyEngNameRule.php
+++ app/Rules/User/OnlyEngNameRule.php
@@ -9,7 +9,7 @@ readonly class OnlyEngNameRule implements InvokableRule
     public function __invoke($attribute, $value, $fail)
     {
         if (!preg_match('/^[A-aZ-z0-9]+$/', $value)) {
-            $fail('Имя должно содержать только английские символы');
+            $fail(__('validation.unique', ['attribute' => __('validation.attributes.subaccount')]));
         }
     }
 }
