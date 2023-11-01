5597cbd2d661b08355faa6b7291d950fae118081
macman <vasily-bazhenov.03@mail.ru>
2023-11-01 12:35:39 +0300

    refactor: form module, mixins && wallets

---
+++ resources/js/modules/form/Components/UI/FormPopupInput.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="form__row">
+		<div class="form__block">
+			<label for="" class="form_label"></label>
+			<input type="text" class="form_input">
+		</div>
+		<div class="form__list-errors">
+			<span class="form_error"></span>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "FormPopupInput",
+}
+</script>
+
+<style scoped lang="scss">
+.form {
+	&__row {
+		@include columnMixin($gap: 8);
+
+	}
+}
+</style>
