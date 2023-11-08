6db7606ea7f61a1bbd7e08cf37fc0339a9517da8
gai_palyan <chadreed020788@yandex.ru>
2023-11-08 19:39:36 +0300

    fix: finance records

--- tests/Feature/Services/IncomeServiceTest.php
+++ tests/Feature/Services/IncomeServiceTest.php
@@ -16,16 +16,11 @@ use App\Services\Internal\IncomeService;
 use App\Services\External\BtcComService;
 use App\Exceptions\IncomeCreatingException;
 use Illuminate\Database\Eloquent\Factories\Sequence;
-use Illuminate\Support\Carbon;
 use Tests\TestCase;
 
 class IncomeServiceTest extends TestCase
 {
-    public Sub $subWithHashRate;
-    public Sub $subWithoutHashRate;
     public MinerStat $stat;
-    public User $referrer;
-    public Sub $referrerSub;
 
     protected function setUp(): void
     {
@@ -42,7 +37,7 @@ class IncomeServiceTest extends TestCase
     public function it_failed_if_sub_without_hash_rate()
     {
         $this->expectException(IncomeCreatingException::class);
-        resolve(IncomeService::class)->init($this->subWithoutHashRate, null);
+        resolve(IncomeService::class)->init(Sub::find(5), null);
     }
 
     /**
@@ -50,20 +45,22 @@ class IncomeServiceTest extends TestCase
      */
     public function it_create_mining_income_and_update_sub_accounts()
     {
+        $subsCollection = Sub::hashWorkerHashRate()->get();
 
-        $subWithHashRate = Sub::hasWorkerHashRate()->first();
-        dd($subWithHashRate);
-        $service = resolve(IncomeService::class)->init($this->subWithHashRate, null);
+        $subWithHashRate = $subsCollection->where('group_id', 6)->first();
 
-        $service->createIncome($this->subWithHashRate, Type::MINING);
-        $service->updateLocalSub($this->subWithHashRate, Type::MINING);
+        $service = resolve(IncomeService::class)->init($subWithHashRate, null);
+
+        $service->createIncome($subWithHashRate, Type::MINING);
+        $service->updateLocalSub($subWithHashRate, Type::MINING);
         $service->createFinance();
 
-        $hashrate = $this->subWithHashRate->total_hash_rate;
+        $hashrate = $subWithHashRate->total_hash_rate;
+        dd($hashrate);
         $expectFirstDayAmount = $this->getDailyAmount($hashrate, BtcComService::FEE + 3.5);
 
         $this->assertDatabaseHas('incomes', [
-            'group_id' => $this->subWithHashRate->group_id,
+            'group_id' => $subWithHashRate->group_id,
             'type' => Type::MINING->value,
             'daily_amount' => $expectFirstDayAmount,
             'status' => Status::PENDING->value,
@@ -71,28 +68,28 @@ class IncomeServiceTest extends TestCase
             'hash' => $hashrate,
         ]);
         $this->assertDatabaseHas('subs', [
-            'user_id' => $this->subWithHashRate->user_id,
-            'group_id' => $this->subWithHashRate->group_id,
-            'sub' => $this->subWithHashRate->sub,
+            'user_id' => $subWithHashRate->user_id,
+            'group_id' => $subWithHashRate->group_id,
+            'sub' => $subWithHashRate->sub,
             'pending_amount' => $expectFirstDayAmount,
             'total_amount' => $expectFirstDayAmount,
         ]);
 
-        $this->subWithHashRate->refresh();
+        $subWithHashRate->refresh();
 
         $this->createAdditionalWorkers();
-        $hashrate = $this->subWithHashRate->total_hash_rate;
+        $hashrate = $subWithHashRate->total_hash_rate;
         $expectSecondDayAmount = $this->getDailyAmount($hashrate, BtcComService::FEE + 3.5);
 
 
-        $service = resolve(IncomeService::class)->init($this->subWithHashRate, null);
+        $service = resolve(IncomeService::class)->init($subWithHashRate, null);
 
-        $service->createIncome($this->subWithHashRate, Type::MINING);
-        $service->updateLocalSub($this->subWithHashRate, Type::MINING);
+        $service->createIncome($subWithHashRate, Type::MINING);
+        $service->updateLocalSub($subWithHashRate, Type::MINING);
         $service->createFinance();
 
         $this->assertDatabaseHas('incomes', [
-            'group_id' => $this->subWithHashRate->group_id,
+            'group_id' => $subWithHashRate->group_id,
             'type' => Type::MINING->value,
             'daily_amount' => $expectSecondDayAmount,
             'status' => Status::READY_TO_PAYOUT->value,
@@ -100,9 +97,9 @@ class IncomeServiceTest extends TestCase
             'hash' => $hashrate,
         ]);
         $this->assertDatabaseHas('subs', [
-            'user_id' => $this->subWithHashRate->user_id,
-            'group_id' => $this->subWithHashRate->group_id,
-            'sub' => $this->subWithHashRate->sub,
+            'user_id' => $subWithHashRate->user_id,
+            'group_id' => $subWithHashRate->group_id,
+            'sub' => $subWithHashRate->sub,
             'pending_amount' => $expectFirstDayAmount + $expectSecondDayAmount,
             'total_amount' => $expectFirstDayAmount + $expectSecondDayAmount,
         ]);
@@ -238,7 +235,15 @@ class IncomeServiceTest extends TestCase
                         'password' => bcrypt('password'),
                         'referral_percent' => 1,
                         'referral_discount' => 0,
-                    ]
+                    ],
+                    [
+                        'id' => 3,
+                        'name' => "CommonSub",
+                        'email' => 'first@gmail.com',
+                        'password' => bcrypt('password'),
+                        'referral_percent' => 0,
+                        'referral_discount' => 0,
+                    ],
                 )
             )->create();
 
@@ -248,8 +253,6 @@ class IncomeServiceTest extends TestCase
                     [
                         'group_id' => 1,
                         'sub' => 'Referrer',
-                        'created_at' => now(),
-                        'updated_at' => now(),
                         'allbtc_fee' => 3.5,
                         'pending_amount' => 0,
                         'is_active' => true,
@@ -259,8 +262,6 @@ class IncomeServiceTest extends TestCase
                     [
                         'group_id' => 2,
                         'sub' => 'Referrer2',
-                        'created_at' => now(),
-                        'updated_at' => now(),
                         'allbtc_fee' => 3.5,
                         'is_active' => false,
                         'pending_amount' => 0,
@@ -270,8 +271,6 @@ class IncomeServiceTest extends TestCase
                     [
                         'group_id' => 3,
                         'sub' => 'Referral',
-                        'created_at' => now(),
-                        'updated_at' => now(),
                         'allbtc_fee' => 3.5,
                         'is_active' => true,
                         'pending_amount' => 0,
@@ -281,13 +280,20 @@ class IncomeServiceTest extends TestCase
                     [
                         'group_id' => 4,
                         'sub' => 'Referral2',
-                        'created_at' => now(),
-                        'updated_at' => now(),
                         'allbtc_fee' => 3.5,
                         'is_active' => false,
                         'pending_amount' => 0,
                         'total_amount' => 0,
                         'user_id' => 2,
+                    ],
+                    [
+                        'group_id' => 6,
+                        'sub' => 'CommonSub',
+                        'allbtc_fee' => 3.5,
+                        'pending_amount' => 0,
+                        'is_active' => true,
+                        'total_amount' => 0,
+                        'user_id' => 3,
                     ],
                 )
             )->create();
@@ -309,6 +315,11 @@ class IncomeServiceTest extends TestCase
                         'status' => 'INACTIVE',
                         'approximate_hash_rate' => 0,
                         'group_id' => 4,
+                    ],
+                    [
+                        'status' => 'ACTIVE',
+                        'approximate_hash_rate' => 100,
+                        'group_id' => 6,
                     ]
                 )
             )->create();
