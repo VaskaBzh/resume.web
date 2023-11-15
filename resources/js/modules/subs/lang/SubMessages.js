5796aa88a08e6fa92287c6f19e9827a2b2e630b0
macman <vasily-bazhenov.03@mail.ru>
2023-11-15 17:11:57 +0300

    feat: add sub cards hints

--- resources/js/modules/subs/lang/SubMessages.js
+++ resources/js/modules/subs/lang/SubMessages.js
@@ -9,17 +9,26 @@ export const SubMessages = {
                 "Current hashrate",
                 "Hashrate / 24h",
                 "Total paid out",
-            ]
+            ],
         },
         no_information: "No results found for your query",
         info_blocks: {
             hash: {
-                titles: ["Current hashrate", "Wed.hashrate / 24"],
+                titles: ["Current Hashrate", "Avg. Hashrate / 24h"],
+                hints: [
+                    "The total current hashrate from all your sub-accounts.",
+                    "The total hashrate over the last 24 hours from all your sub-accounts.",
+                ],
             },
             workers: {
-                titles: "Workers",
+                title: "Workers",
                 types: ["Active", "Unstable", "Inactive"],
-            }
+                hints: {
+                    active: "The sum of active workers from all your sub-accounts.",
+                    inactive:
+                        "The sum of inactive workers from all your sub-accounts.",
+                },
+            },
         },
     },
     ru: {
@@ -32,17 +41,26 @@ export const SubMessages = {
                 "Текущий хешрейт",
                 "Хешрейт / 24ч",
                 "Выплачено всего",
-            ]
+            ],
         },
         no_information: "По вашему запросу ничего не найдено",
         info_blocks: {
             hash: {
                 titles: ["Текущий хешрейт", "Ср.Хешрейт / 24"],
+                hints: [
+                    "Общий текущий хешрейт со всех ваших субаккаунтов.",
+                    "Общий хешрейт за 24 часа со всех ваших субаккаунтов.",
+                ],
             },
             workers: {
                 title: "Воркеры",
                 types: ["Активные", "Нестабильные", "Неактивные"],
+                hints: {
+                    active: "Сумма активных воркеров со всех ваших субаккаунтов.",
+                    inactive:
+                        "Сумма неактивных воркеров со всех ваших субаккаунтов.",
+                },
             },
-        }
-    }
-}
+        },
+    },
+};
