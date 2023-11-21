4f98521179ed570c792b3e72af2f88e0dac24b92
gai_palyan <chadreed020788@yandex.ru>
2023-11-21 18:15:23 +0300

    fix: worker & subs hashes per min type, createing command

--- app/Http/Resources/HashRateResource.php
+++ app/Http/Resources/HashRateResource.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Resources;
 
+use App\Utils\HashRateConverter;
 use Illuminate\Http\Resources\Json\JsonResource;
 use OpenApi\Attributes as OA;
 
@@ -22,9 +23,11 @@ class HashRateResource extends JsonResource
 {
     public function toArray($request): array
     {
+        $hashRate = HashRateConverter::fromPure($this->hash);
+
         return [
-            'hash' => $this->hash,
-            'unit' => $this->unit,
+            'hash' => $hashRate->value,
+            'unit' => $hashRate->unit,
             'worker_count' => $this->worker_count,
         ];
     }
