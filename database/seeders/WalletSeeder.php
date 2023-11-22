c4c139f78cae85a630ad564d91cf7c8384d55a26
gai_palyan <chadreed020788@yandex.ru>
2023-11-22 14:14:08 +0300

    feat: test data seeders

---
+++ database/seeders/WalletSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Wallet;
+use Illuminate\Database\Seeder;
+
+class WalletSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Wallet::firstOrCreate(['group_id' => 6001912], [
+            [
+                'name' => 'wallet',
+                'minWithdrawal' => 0,
+                'wallet' => 'wallet',
+                'percent' => 100,
+                'wallet_updated_at' => now()->subDays(2),
+            ],
+        ]);
+    }
+}
