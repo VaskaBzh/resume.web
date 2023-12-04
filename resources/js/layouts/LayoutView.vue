03d634d802489604fdb07a80984c41582a912eb2
GlazGo31 <job.glazunov@gmail.com>
2023-12-04 15:39:35 +0300

    fix: scroll faq

--- resources/js/layouts/LayoutView.vue
+++ resources/js/layouts/LayoutView.vue
@@ -59,7 +59,7 @@ export default {
 
 .layout_button {
     border: 0.5px solid rgba(192, 228, 255, 0.6);
-    background: var(--gray-470, rgba(13, 13, 13, 0.7));
+    background: rgba(13, 13, 13, 0.7);
     backdrop-filter: blur(10px);
     width: clamp(40px, 3vw, 80px);
     height: clamp(40px, 3vw, 80px);
@@ -67,7 +67,7 @@ export default {
     display: inline-flex;
     align-items: center;
     justify-content: center;
-    color: var(--gray-2100, #e4e7ec);
+    color: #e4e7ec;
     font-family: Unbounded, serif;
     font-size: clamp(20px, 5vw, 32px);
     font-weight: 400;
