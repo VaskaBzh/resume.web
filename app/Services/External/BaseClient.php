3f062d805f49154a037a46d7cdd6cba558c28e94
gai_palyan <chadreed020788@yandex.ru>
2023-12-07 10:55:54 +0300

    feat: api interface]

---
+++ app/Services/External/BaseClient.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\External;
+
+use App\Utils\ApiRequest;
+use Illuminate\Http\Client\PendingRequest;
+use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\Http;
+
+abstract class BaseClient
+{
+    public function send(ApiRequest $request): Response
+    {
+        return $this->getBaseRequest()
+            ->withHeaders($request->getHeaders())
+            ->{$request->getMethod()}(
+                uri: $request->getUri(),
+                method: $request->getMethod() === 'get'
+                    ? $request->getQuery()
+                    : $request->getBody()
+            );
+    }
+
+    /**
+     * Get a base request for the API.
+     */
+    protected function getBaseRequest(): PendingRequest
+    {
+        $request = Http::acceptJson()
+            ->contentType('application/json')
+            ->throw()
+            ->baseUrl($this->baseUrl());
+
+        return $this->authorize($request);
+    }
+
+    /**
+     * Authorize a request for the API.
+     */
+    protected function authorize(PendingRequest $request): PendingRequest
+    {
+        return $request;
+    }
+
+    /**
+     * Get the base URL for the API.
+     */
+    abstract protected function baseUrl(): string;
+}
