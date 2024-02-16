e58ebe0e5fa299807ebc5ee1ee38ff4b0f2dcdff
peper0n <dimakoncha@gmail.com>
2024-02-16 15:21:48 +0800

    Added

--- tests/Feature/Services/IncomeServiceTest.php
+++ tests/Feature/Services/IncomeServiceTest.php
@@ -193,7 +193,8 @@ class IncomeServiceTest extends TestCase
     public function referralDiscountCase()
     {
         $referralSub = $this->subsWithHashRate->where('group_id', 3)->first();
-        $referralSub->user->update(['referral_discount' => 6]);
+        $referralSub->user->update(['referral_discount' => 0]);
+        $referralSub->user->update(['referrer_id' => null]);
         $referralSub->wallets->first()->update(['wallet_updated_at' => now()->subHours(48)]);
 
         $service = IncomeService::init($this->stat, $referralSub, null);
@@ -209,15 +210,12 @@ class IncomeServiceTest extends TestCase
             '.',
             ''
         );
-
         if($referralSub->user->referrer_id == null ){
             $clear_percent = config('api.btc.fee');
         } else {
-            $clear_percent = 10 - $referralSub->user->referral_percent -
+            $clear_percent = config('api.btc.fee') - $referralSub->user->referral_percent -
                 $referralSub->user->referral_discount;
         }
-
-//        dd($clear_percent, $referralSub->user->referral_percent, $referralSub->user->referral_discount, $pureDailyEarning, $pureDailyEarning * ($clear_percent / 100));
         $service->createIncome($referralSub, Type::MINING);
         $service->updateLocalSub($referralSub, Type::MINING);
         $service->createFinance();
