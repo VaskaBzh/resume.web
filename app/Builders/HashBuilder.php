4bad5a0e6b98ee823e4d69508f253885b9ab8771
peper0n <dimakoncha@gmail.com>
2024-02-29 20:27:36 +0800

    feat: day graph update

--- app/Builders/HashBuilder.php
+++ app/Builders/HashBuilder.php
@@ -60,62 +60,30 @@ class HashBuilder extends BaseBuilder
 
             case 'month':
 
-//                $records = DB::select(
-//                    "SELECT h.*
-//    FROM hashes h
-//JOIN (
-//    SELECT MIN(id) AS min_id
-//    FROM (
-//        SELECT id,
-//            created_at,
-//            CASE
-//                WHEN HOUR(created_at) < 8 THEN '00:00'
-//                WHEN HOUR(created_at) < 18 THEN '08:00'
-//                ELSE '16:00'
-//            END AS time_slot
-//        FROM hashes
-//        WHERE created_at >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH)
-//        AND created_at < CURDATE()
-//    ) AS filtered_hashes
-//    GROUP BY DATE(created_at), time_slot
-//) AS min_ids ON h.id = min_ids.min_id;"
-//                );
-
                 DB::statement("SET @row_number := 0");
-                $query = "
-    SELECT rounded_created_at,
-           MIN(hash) AS hash,
-           MAX(unit) AS unit,
-           MAX(worker_count) AS worker_count
+                $query = "SELECT h.*
+    FROM hashes h
+JOIN (
+    SELECT MIN(id) AS min_id
     FROM (
-        SELECT *, (@row_number:=@row_number + 1) AS rn
-        FROM (
-            SELECT MIN(hash) AS hash,
-                   DATE_FORMAT(created_at, '%Y-%m-%d %H:00') AS rounded_created_at,
-                   unit,
-                   worker_count
-            FROM hashes
-            WHERE created_at >= CURDATE() - INTERVAL 30 DAY
-            GROUP BY rounded_created_at, unit, worker_count
-        ) AS unique_hashes
-        ORDER BY rounded_created_at DESC
-    ) AS init
-    WHERE HOUR(rounded_created_at) % 8 = 0
-    GROUP BY rounded_created_at;
-";
+        SELECT id,
+            created_at,
+            CASE
+                WHEN HOUR(created_at) < 8 THEN '00:00'
+                WHEN HOUR(created_at) < 18 THEN '08:00'
+                ELSE '16:00'
+            END AS time_slot
+        FROM hashes
+        WHERE created_at >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH)
+        AND created_at < CURDATE()
+    ) AS filtered_hashes
+    GROUP BY DATE(created_at), time_slot
+) AS min_ids ON h.id = min_ids.min_id;";
+
                 $response = DB::select($query);
-
-                dd($response);
                 $hashes = Hash::hydrate($response);
-
 
                 return $hashes;
         }
-    }
-    public function fillHashesToMatrix($hashes, $dateTimeMatrix) {
-        return array_reduce($hashes, function ($carry, $item) {
-            $carry[$item->rounded_created_at] = $item->hash;
-            return $carry;
-        }, $dateTimeMatrix);
     }
 }
